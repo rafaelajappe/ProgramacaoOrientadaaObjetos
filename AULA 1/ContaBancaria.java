46f27e5bd9c8808539625b3a527ca763341e2662
rafaelajappe <japperafaela@gmail.com>
2026-08-10 23:59:12 -0300

    Update ContaBancaria.java

--- AULA 1/ContaBancaria.java
+++ AULA 1/ContaBancaria.java
@@ -1,7 +1,48 @@
 #exercicio 3
 
-public class PrincipalConta {
-    public static void main(String[] args) {
-        
-}
+public class ContaBancaria {
 
+    String titular;
+    double saldo;
+
+    public ContaBancaria(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    // Método para depositar
+    public void depositar(double valor) {
+        saldo = saldo + valor;
+        System.out.println("Depósito realizado com sucesso!");
+    }
+
+    // Método para sacar
+    public void sacar(double valor) {
+
+        if (valor <= saldo) {
+            saldo = saldo - valor;
+            System.out.println("Saque realizado com sucesso!");
+        } else {
+            System.out.println("Saque não realizado! Saldo insuficiente.");
+        }
+    }
+
+    // Método para exibir o saldo
+    public void exibirSaldo() {
+        System.out.println("Titular: " + titular);
+        System.out.println("Saldo: R$ " + saldo);
+        System.out.println("----------------------");
+    }
+
+    // Método para transferir
+    public void transferir(ContaBancaria destino, double valor) {
+
+        if (valor <= saldo) {
+            sacar(valor);
+            destino.depositar(valor);
+            System.out.println("Transferência realizada com sucesso!");
+        } else {
+            System.out.println("Transferência não realizada! Saldo insuficiente.");
+        }
+    }
+}
