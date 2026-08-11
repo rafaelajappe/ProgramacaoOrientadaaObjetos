ed8e40d762e056a8409d1195d0f3fea1387bddf1
rafaelajappe <japperafaela@gmail.com>
2026-08-10 22:03:01 -0300

    Create Principal.java

---
+++ AULA 1/Principal.java
@@ -0,0 +1,17 @@
+#EXERCICIO 01
+
+public class Principall {
+    public static void main (String[] args) {
+        Produto p1 = new Produto();
+        p1.nome = "mouse gamer";
+        p1.estoque = 50;
+        p1.preco = 560.00;
+
+        p1.exibirInfo();
+        p1.vender(quant: 30);
+        p1.vender(quant:30);
+
+        Produto p2 = new Produto();
+        p2.nome = 
+        
+}
