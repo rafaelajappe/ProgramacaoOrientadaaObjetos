a8d71748f34c54207558f9a30daa570d0e13c492
rafaelajappe <japperafaela@gmail.com>
2026-08-10 23:55:21 -0300

    Update Principal.java

--- AULA 1/Principal.java
+++ AULA 1/Principal.java
@@ -1,17 +1,20 @@
 #EXERCICIO 01
 
-public class Principall {
-    public static void main (String[] args) {
-        Produto p1 = new Produto();
-        p1.nome = "mouse gamer";
-        p1.estoque = 50;
-        p1.preco = 560.00;
+public class Principal {
 
-        p1.exibirInfo();
-        p1.vender(quant: 30);
-        p1.vender(quant:30);
+    public static void main(String[] args) {
 
-        Produto p2 = new Produto();
-        p2.nome = 
-        
+        // Criando 3 produtos
+        Produto produto1 = new Produto("Notebook", 3500.00, 10);
+        Produto produto2 = new Produto("Mouse", 80.00, 5);
+        Produto produto3 = new Produto("Teclado", 150.00, 3);
+
+        // Vendendo 2 unidades do produto 1
+        produto1.vender(2);
+
+        // Exibindo todos os produtos
+        produto1.exibirInfo();
+        produto2.exibirInfo();
+        produto3.exibirInfo();
+    }
 }
