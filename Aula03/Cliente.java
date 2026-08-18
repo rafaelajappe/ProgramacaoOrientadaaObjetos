04f95e849d40b8c78083e5a4c3e9dbd8684f2250
rafaelajappe <japperafaela@gmail.com>
2026-08-17 23:54:13 -0300

    Create Cliente.java

---
+++ Aula03/Cliente.java
@@ -0,0 +1,10 @@
+#exercicio 1
+public class Cliente {
+    String nome;
+    String email;
+
+    Cliente(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+}
