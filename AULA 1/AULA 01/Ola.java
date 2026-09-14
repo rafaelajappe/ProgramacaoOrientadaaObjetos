ffd96d02042e49fdbe4ac7a319ac60d1dd816666
rafaelajappe <japperafaela@gmail.com>
2026-09-14 00:20:36 -0300

    Add files via upload

---
+++ AULA 1/AULA 01/Ola.java
@@ -0,0 +1,9 @@
+public class Ola {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá, meu nome é Rafaela Jappe!");
+        System.out.println("Meu curso é Sistemas de Informação.");
+
+    }
+}
